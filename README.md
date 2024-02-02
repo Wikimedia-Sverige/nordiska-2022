a20899f5b6fd2f559c01b72b4bcbb89021c10261
lokal-profil <lokal.profil@gmail.com>
2024-02-02 08:02:09 +0100

    Add diff stats and final commons-diff output

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ Below is a very brief description of each script.
 * **wp_captions**: Get all images in a Commons category and for each get all global usages and associated captions.
 * **deriv_detector**: Attempt to identify derivative files by analysing which other files link to files in a Commons category.
 * **file_count**: Show the growth of a Commons category of files by analysing the upload dates of its category.
+* **diff_stats**: Extract some statistics from the [commons-diff](https://github.com/Wikimedia-Sverige/commons-diff/) output files. The `data` directory also contains the final commons-diff output.
 
 ## Disclaimer
 
---
+++ diff_stats/get_diff_stats.py
@@ -0,0 +1,62 @@
+"""Takes a commons-diff output file and outputs some statistics."""
+import json
+
+def check_changes(d, key, stats_data):
+    key_data = d.get(key)
+    if(key_data.get('added') or key_data.get('removed')):
+        stats_data[key] += len(key_data.get('added')) - len(key_data.get('removed'))
+        return True
+    return False
+
+def analyse_data(data):
+    stats = {
+        'changed': 0,
+        'captions': 0,
+        'categories': 0,
+        'descriptions': 0,  # counts if changed, not number of additions
+        'statements': 0,
+        'all': 0
+    }
+
+    languages = set()
+    for d in data:
+        stats['all'] += 1
+        change = False
+        change = change or check_changes(d, 'captions', stats)
+        change = change or check_changes(d, 'categories', stats)
+        change = change or check_changes(d, 'statements', stats)
+
+        if d.get('description').get('changed'):
+            change = True
+            stats['descriptions'] += 1
+
+        if change:
+            stats['changed'] += 1
+
+        # check added caption languages
+        added_captions = d.get('captions').get('added')
+        languages.update([list(val.keys())[0] for val in added_captions])
+
+    stats['caption_languages'] = len(languages)
+    return stats
+
+def output_stats(stats, filename, meta):
+    outfile = f'{filename.partition(".")[0]}_stats.json'
+    data = {'meta': meta, 'stats': stats}
+    with open(outfile, 'w', encoding ='utf8') as fp:
+        json.dump(data, fp, sort_keys=True, indent=2, ensure_ascii=False)
+    print(f'Outputted stats to {outfile}')
+
+def process_output(filename):
+    results = None
+
+    with open(filename, encoding='utf8') as f:
+        results = json.load(f)
+
+    meta = results.get('meta')
+    stats = analyse_data(results.get('results'))
+    output_stats(stats, filename, meta)
+
+
+input_file = input('path to output file: ')
+process_output(input_file)
